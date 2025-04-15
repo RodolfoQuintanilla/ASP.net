bb2bbae4085add4ff7dda195db2e2621d1d41ff7
Rodolfo <rqm@outlook.es>
2025-04-14 22:20:49 -0600

    Inicio

--- src/AppStore/Models/Domain/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace AppStore.Models.Domain;
-
-public class ApplicationUser : IdentityUser
-{
-    public string? Nombre {get;set;}
-}
--- src/AppStore/obj/Debug/net8.0/AppStore.AssemblyInfo.cs
+++ src/AppStore/obj/Debug/net8.0/AppStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21b62ba4b823b8519653b09a80d7a5e7373f77e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
