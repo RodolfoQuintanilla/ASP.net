c53f741ac0db8de4246dfada9095a17861fe035e
Rodolfo <rqm@outlook.es>
2025-04-19 19:15:47 -0600

    fin Migration

--- src/AppStore/Program.cs
+++ src/AppStore/Program.cs
@@ -1,7 +1,28 @@
+using AppStore.Models.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddDbContext<DatabaseContext>(opt =>
+{
+    opt.LogTo(Console.WriteLine, new[] {
+        DbLoggerCategory.Database.Command.Name},
+        LogLevel.Information).EnableSensitiveDataLogging();
+
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteDatabase"));
+});
+
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+.AddEntityFrameworkStores<DatabaseContext>()
+.AddDefaultTokenProviders();
+
 
 var app = builder.Build();
 
@@ -18,10 +39,34 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+using (var ambiente = app.Services.CreateScope())
+{
+
+    var services = ambiente.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DatabaseContext>();
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await context.Database.MigrateAsync();
+        
+        await LoadDatabase.InsertarData(context, userManager, roleManager);
+    }
+    catch (Exception e)
+    {
+        var logging = services.GetRequiredService<ILogger<Program>>();
+        logging.LogError(e, "Ocurrio un error en la insercion de datos");
+    }
+}
+
 
 app.Run();
